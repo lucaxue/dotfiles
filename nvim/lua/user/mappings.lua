7075e2b5da9f9246be7bd7feb106e7e9ac4a6964
Luca Xue <lxue02@gmail.com>
2022-03-27 17:16:38 +0100

    Add mapping to preserve visual section after indentation

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,4 +1,9 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('i', 'jk', '<Esc>', opts) -- jk to escape
+-- Press jk to espace
+map("i", "jk", "<ESC>", opts)
+
+-- Preserve visual selection after indentation
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
