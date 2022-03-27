fc698710a668ea52f9cdafb047f72c1095df2068
Luca Xue <lxue02@gmail.com>
2022-03-27 13:39:16 +0100

    Move plugins config into its own modules

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,10 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Let packer manage itself
 
-  use "projekt0n/github-nvim-theme"
+  use {
+      'projekt0n/github-nvim-theme',
+      config = function()
+          require('plugins.github-theme')
+      end
+  }
 end)
