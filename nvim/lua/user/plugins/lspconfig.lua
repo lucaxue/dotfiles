757076dee2df4d72282dfea2ea96f5ec26d065ae
Luca Xue <lxue02@gmail.com>
2022-04-02 12:03:32 +0100

    Add rounded border for lsp suggestions

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,13 +1,9 @@
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -29,12 +25,28 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+local border = {
+  {'╭', 'FloatBorder'},
+  {'─', 'FloatBorder'},
+  {'╮', 'FloatBorder'},
+  {'│', 'FloatBorder'},
+  {'╯', 'FloatBorder'},
+  {'─', 'FloatBorder'},
+  {'╰', 'FloatBorder'},
+  {'│', 'FloatBorder'},
+}
+local handlers =  {
+  ['textDocument/hover'] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+  ['textDocument/signatureHelp'] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { 'intelephense' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
+    handlers = handlers,
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
